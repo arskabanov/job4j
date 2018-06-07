22aa8d71a968688f9af84b7fe953eedb0345b54b
arskabanov <mailto:gedeon50@yandex.ru>
2018-06-07 22:25:50 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,18 @@
-package ru.job4j.calculate;
+﻿package ru.job4j.calculate;
 
+/**
+* Class Класс пока для вывода текста.
+* В дальнейшем для арифметических вычеслений
+* @author arskabanov
+* @since 07.06.2018
+* @version 1
+*/
 public class Calculate {
 	
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
-
+	/**
+* Тут будет описание метода
+*/
 }
