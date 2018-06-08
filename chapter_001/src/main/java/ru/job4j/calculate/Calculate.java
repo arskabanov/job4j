d9ab055a23bffc2b28e35694076125f56e16c8f8
arskabanov <mailto:gedeon50@yandex.ru>
2018-06-08 23:32:39 +0300

    1.3.1 Maven Plugins [#49546]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,4 @@
-﻿package ru.job4j.calculate;
-
+package ru.job4j.calculate;
 /**
 * Class Класс пока для вывода текста.
 * В дальнейшем для арифметических вычеслений
--- chapter_001/src/main/java/ru/job4j/calculate/package-info.java
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -1,8 +1,7 @@
-﻿/**
-* Package for calculate task.
-*
-* @author Arseniy Kabanov (mailto:gedeon@yandex.ru)
-* @since 07.06.2018
-* @version 0.1
+/**
+ * Package for calculate task.
+ * @author Arseniy Kabanov (mailto:gedeon@yandex.ru)
+ * @since 07.06.2018
+ * @version 0.1
 */
 package ru.job4j.calculate;
